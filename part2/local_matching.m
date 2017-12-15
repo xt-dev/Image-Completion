b91b30fc53be6bc8caacb37969dddc0ecd251dbc
Suyie <qilezhi2@illinois.edu>
2017-12-14 23:56:49 -0600

    out3 done

--- part2/local_matching.m
+++ part2/local_matching.m
@@ -3,7 +3,9 @@ function local_matching(~)
 	% starter code for local context matching
 	close all
 
-	local_matching_single('../gist_res/test1 copy', false);
 % 	local_matching_single('../gist_res/test2', true);
+% 	local_matching_single('../gist_res/test1', false);
+% 	local_matching_single('../gist_res/test1 copy', false);
+	local_matching_single('../out3', true);
 
 end
